82ee07577ad5e9a462ae3cbf4acc57a5d3d9cd97
Samuel Jeffery <nomadicsamuel@gmail.com>
2026-03-21 11:29:30 -0300

    Update migrate_argentina_drip.py

--- migrate_argentina_drip.py
+++ migrate_argentina_drip.py
@@ -288,7 +288,7 @@ def process_album_images(images, official_album_name, global_count, processed_hi
             else:
                 print_now(f"  ⚠️ SmugMug Update Failed: {patch_resp.status_code}")
 
-            time.sleep(random.randint(31, 51)) 
+            time.sleep(41)
         except Exception as e:
             print_now(f"  ❌ Failed: {e}")
         finally:
